5e9bef8efbaedf4c4c3c33d202f99ffc49f6a6fa
Vlad Nicu <Vlad.Nicu@trw.com>
2020-10-23 19:28:25 +0300

    added ingredients model, routes, dummy controllers, requests, resources

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\IngredientController;
 use App\Http\Controllers\ReceipeController;
+use App\Http\Controllers\StepController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,4 +27,20 @@ Route::group(['prefix' => 'receipes'], function () {
     Route::post('/', [ReceipeController::class, 'store']);
     Route::patch('/{receipe}', [ReceipeController::class, 'update']);
     Route::delete('/{receipe}', [ReceipeController::class, 'delete']);
+
+    Route::group(['prefix' => '/{receipe}/ingredients'], function() {
+        Route::get('/', [IngredientController::class, 'index']);
+        Route::get('/{ingredient}', [IngredientController::class, 'show']);
+        Route::post('/', [IngredientController::class, 'store']);
+        Route::patch('/{ingredient}', [IngredientController::class, 'update']);
+        Route::delete('/{ingredient}', [IngredientController::class, 'delete']);
+    });
+
+    Route::group(['prefix' => '/{receipe}/steps'], function() {
+        Route::get('/', [StepController::class, 'index']);
+        Route::get('/{step}', [StepController::class, 'show']);
+        Route::post('/', [StepController::class, 'store']);
+        Route::patch('/{step}', [StepController::class, 'update']);
+        Route::delete('/{step}', [StepController::class, 'delete']);
+    });
 });
