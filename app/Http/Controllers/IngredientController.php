655afa2ce37aa13281b622852e4ea627c69f5f8e
Vlad Nicu <Vlad.Nicu@trw.com>
2020-10-23 19:32:34 +0300

    added steps and ingredients CRUD methods

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreIngredientRequest;
+use App\Http\Requests\UpdateIngredientRequest;
+use App\Http\Resources\IngredientResource;
+use App\Models\Ingredient;
+use App\Models\Receipe;
 use Illuminate\Http\Request;
 
 class IngredientController extends Controller
 {
-    //
+    public function store(StoreIngredientRequest $request, Receipe $receipe) {
+        $ingredient = new Ingredient();
+        $ingredient->name = $request->name;
+   
+
+        $receipe->ingredients()->save($ingredient);
+
+        return new IngredientResource($ingredient);
+    }
+
+    public function show(Ingredient $ingredient) {
+     
+        return new IngredientResource($ingredient);
+    }
+
+    public function update(UpdateIngredientRequest $request, Receipe $receipe, Ingredient $ingredient) {
+
+        $ingredient->name = $request->get('name', $ingredient->name); //keep the same value if the value isn't provided
+        $ingredient->save();
+
+        return new IngredientResource($ingredient);
+    }
+
+    public function delete(Receipe $receipe, Ingredient $ingredient) {
+
+        $ingredient->delete();
+
+        return response(null, 204);
+    }
 }
--- app/Http/Controllers/StepController.php
+++ app/Http/Controllers/StepController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStepRequest;
+use App\Http\Requests\UpdateStepRequest;
+use App\Http\Resources\StepResource;
+use App\Models\Receipe;
+use App\Models\Step;
 use Illuminate\Http\Request;
 
 class StepController extends Controller
 {
-    //
+    public function store(StoreStepRequest $request, Receipe $receipe) {
+        $step = new Step();
+        $step->name = $request->name;
+   
+
+        $receipe->steps()->save($step);
+
+        return new StepResource($step);
+    }
+
+    public function show(Step $step) {
+     
+        return new StepResource($step);
+    }
+
+    public function update(UpdateStepRequest $request, Receipe $receipe, Step $step) {
+
+        $step->name = $request->get('name', $step->name); //keep the same value if the value isn't provided
+        $step->save();
+
+        return new StepResource($step);
+    }
+
+    public function delete(Receipe $receipe, Step $step) {
+
+        $step->delete();
+
+        return response(null, 204);
+    }
 }
